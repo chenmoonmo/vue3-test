598a86c49c2223adf87fdf6ea70432bfaa063a03
chenmoonmo <chenmoonmo@gmail.com>
2021-01-24 13:31:31 +0800

    尝试setup函数

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="hello">{{message}}</h1>
+</template>
+
+<script>
+import { toRef, ref } from 'vue'
+export default {
+  props: {
+    name: String
+  },
+  setup(props) {
+    const { name } = toRef(props)
+    console.log(props.name)
+    const message = `hello ${props.name}!`
+    return { message }
+  }
+}
+</script>
+
+<style>
+</style>
